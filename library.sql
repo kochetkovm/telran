23bae137057cd83d7c0ea12de9cd46136fbaef3f
Kochetkov Mihael <jead@list.ru>
2018-10-16 20:50:15 +0300

    First Commit

---
+++ library.sql
@@ -0,0 +1,17 @@
+mysql -u root -p
+
+CREATE database `library`;
+
+USE `library`;
+
+CREATE table `books`(
+`bookid` int(4) unsigned NOT NULL,
+`title` varchar(30) NOT NULL,
+`author` varchar(30) NOT NULL,
+`published` DATETIME(4),
+`stock` int(4)
+);
+
+INSERT INTO `books` VALUES
+(1, 'Scion of Ikshvaku', 'Amish Tripathi', 06-22-2015, 2);
+
